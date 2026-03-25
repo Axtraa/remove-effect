269610b673a33b293484a82ac5ea9052f23c31c7
Axtraa <lucas.benoit2000@gmail.com>
2026-03-25 13:25:16 +0100

    Add No Orb Pulse setting
    - Add boolean setting in mod.json to disable orb pulsing
    - Hook PlayLayer::updateVisibility to control pulse effect
    - Save and restore original pulse values to prevent side effects

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <Geode/Geode.hpp>
+#include <Geode/loader/SettingV3.hpp>
+
+using namespace geode::prelude;
+
+class $modify (PlayLayer) {
+    virtual void updateVisibility(float dt) {
+        auto fmod = FMODAudioEngine::get();
+        auto pulse1 = fmod->m_pulse1;
+        auto audioScale = m_audioEffectsLayer->m_audioScale;
+
+        if (Mod::get()->getSettingValue<bool>("no-orb-pulse")) {
+            fmod->m_pulse1 = 0.7f;
+            m_audioEffectsLayer->m_audioScale = 0.7f;
+        }
+
+        PlayLayer::updateVisibility(dt);
+
+        fmod->m_pulse1 = pulse1;
+        m_audioEffectsLayer->m_audioScale = audioScale;
+    }
+};
