19b7b6b9289e127b6d885fde6c2384b668ce78c4
Axtraa <lucas.benoit2000@gmail.com>
2026-03-27 20:10:20 +0100

    style(pause-menu): reduce button scale to 0.675

--- src/main.cpp
+++ src/main.cpp
@@ -44,7 +44,7 @@ class $modify (RemoveEffectPauseLayer, PauseLayer) {
         auto buttonSprite = CircleButtonSprite::create(
             CCSprite::create("button.png"_spr)
         );
-        buttonSprite->setScale(0.65f);
+        buttonSprite->setScale(0.675f);
 
         auto button = CCMenuItemSpriteExtra::create(
             buttonSprite,
